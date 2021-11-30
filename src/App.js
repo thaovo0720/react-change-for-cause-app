4456d6513e9758fdf1ed03ddeb603ab9380beb21
thaovo0720 <tvo0720@outlook.com>
2021-11-30 10:56:10 -0500

    deploy react app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+//author: Thao Vo-CSIS 473
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+      <h2>Thao Vo</h2>
+    </div>
+  );
+}
+
+export default App;
