a441ab3aeb7613d552c5eb9cd3553ecda2110293
litblurocker <94061701+litblurocker@users.noreply.github.com>
2021-12-03 16:28:10 -0500

    Add files via upload
    start of tests

---
+++ App.test.js
@@ -0,0 +1,17 @@
+const App = require('./App');
+
+test('The admin email address should be correct', () => {
+    
+});
+
+test('The admin password should be correct', () => {
+
+});
+
+test('The admin is logged in', () => {
+
+});
+
+test('The admin is logged out', () => {
+
+});
